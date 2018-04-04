56eafba36c70b29544ac7167b0a9dbaea2bc2176
Andi Dwi Purnama <andipurnama@softwareseni.com>
2018-04-04 16:17:11 +0700

    include parent construct

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -4,9 +4,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Migrate extends CI_Controller
 {
 
-	
 	public function __construct()
 	{
+		parent::__construct();
 		if (!$this->CheckAccess()) {
 			echo 'You are not allowed to access this!';
 			exit();
